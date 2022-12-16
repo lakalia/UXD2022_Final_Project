5199c684b6cada837ae90019566eea0d28de9a53
Nicole Conill <nconill@wpi.edu>
2022-12-15 20:30:54 -0500

    Updates

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,25 @@
-function displaySources() {
-	var x = document.getElementById("sources");
+function displayMobileWireframes() {
+	var x = document.getElementById("mobile-wireframes");
 	if (x.style.display === "none") {
-		x.style.display = "block";
+		x.style.display = "flex";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function displayDesktopWireframes() {
+	var x = document.getElementById("desktop-wireframes");
+	if (x.style.display === "none") {
+		x.style.display = "flex";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function displayTVWireframes() {
+	var x = document.getElementById("tv-wireframes");
+	if (x.style.display === "none") {
+		x.style.display = "flex";
 	} else {
 		x.style.display = "none";
 	}
